4eb43d80b7309f77f0e258fffe7205f7ac955d77
HP <bapinam1998@gmail.com>
2021-11-17 21:05:37 +0700

    efCoreAlbum

---
+++ src/MusicManagement.EntityFrameworkCore/EntityFrameworkCore/Albums/EfCoreAlbumRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Dynamic.Core;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MusicManagement.Albums;
+using Volo.Abp.Domain.Repositories.EntityFrameworkCore;
+using Volo.Abp.EntityFrameworkCore;
+
+namespace MusicManagement.EntityFrameworkCore.Albums
+{
+    public class EfCoreAlbumRepository
+         : EfCoreRepository<MusicManagementDbContext, Album, Guid>,
+             IAlbumRepository
+    {
+        public EfCoreAlbumRepository(
+            IDbContextProvider<MusicManagementDbContext> dbContextProvider)
+            : base(dbContextProvider)
+        {
+        }
+
+        public async Task<Album> FindByNameAsync(string name)
+        {
+            var dbSet = await GetDbSetAsync();
+            return await dbSet.FirstOrDefaultAsync(album => album.Name == name);
+        }
+
+        public async Task<List<Album>> GetListAsync(
+            int skipCount,
+            int maxResultCount,
+            string sorting,
+            string filter = null)
+        {
+            var dbSet = await GetDbSetAsync();
+            return await dbSet
+                .WhereIf(
+                    !filter.IsNullOrWhiteSpace(),
+                    album => album.Name.Contains(filter)
+                 )
+                .OrderBy(sorting)
+                .Skip(skipCount)
+                .Take(maxResultCount)
+                .ToListAsync();
+        }
+    }
+}
